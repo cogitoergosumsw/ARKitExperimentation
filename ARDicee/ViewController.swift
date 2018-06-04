3b8818ed0e64ae83d994d6619fb095a23856cefa
Seng Wee <swong039@e.ntu.edu.sg>
2018-06-04 20:07:12 +0800

    importing a Mars texture and showing the Mars on screen instead of a cube

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -20,15 +20,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        let sphere = SCNSphere(radius: 0.1)
         
         let material = SCNMaterial()
-        material.diffuse.contents = UIColor.red
-        cube.materials = [material]
+        material.diffuse.contents = UIImage(named: "art.scnassets/mars.jpg")
+        sphere.materials = [material]
         
         let node = SCNNode()
-        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
-        node.geometry = cube
+        node.position = SCNVector3(x: 0, y: 0.1, z: -0.4)
+        node.geometry = sphere
         
         sceneView.scene.rootNode.addChildNode(node)
         sceneView.autoenablesDefaultLighting = true
