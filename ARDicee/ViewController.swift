3d0f459e523d8b34455f6d55c7b5728a943c7c5e
Seng Wee <swong039@e.ntu.edu.sg>
2018-06-04 20:25:23 +0800

    added a dice instead of Mars

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -21,24 +21,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         
 //        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-        let sphere = SCNSphere(radius: 0.1)
+//        let sphere = SCNSphere(radius: 0.1)
+//
+//        let material = SCNMaterial()
+//        material.diffuse.contents = UIImage(named: "art.scnassets/mars.jpg")
+//        sphere.materials = [material]
+//
+//        let node = SCNNode()
+//        node.position = SCNVector3(x: 0, y: 0.1, z: -0.4)
+//        node.geometry = sphere
         
-        let material = SCNMaterial()
-        material.diffuse.contents = UIImage(named: "art.scnassets/mars.jpg")
-        sphere.materials = [material]
-        
-        let node = SCNNode()
-        node.position = SCNVector3(x: 0, y: 0.1, z: -0.4)
-        node.geometry = sphere
-        
-        sceneView.scene.rootNode.addChildNode(node)
+//        sceneView.scene.rootNode.addChildNode(node)
         sceneView.autoenablesDefaultLighting = true
         
         // Create a new scene
-        //        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        //
-        //        // Set the scene to the view
-        //        sceneView.scene = scene
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.2)
+            sceneView.scene.rootNode.addChildNode(diceNode)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
